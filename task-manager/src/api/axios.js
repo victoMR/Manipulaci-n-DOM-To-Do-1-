39d871977e17ffb63e55c681d486959fc674ea04
victoMR <ratonrangelmejia+2@gmail.com>
2025-03-09 18:00:25 -0600

    modificacion de la url

--- task-manager/src/api/axios.js
+++ task-manager/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://53fe-2806-264-440e-4660-39c6-94ef-c220-494e.ngrok-free.app/',
+  baseURL: 'https://manipulaci-n-dom-to-do-1.onrender.com',
   withCredentials: true,
 });
 
