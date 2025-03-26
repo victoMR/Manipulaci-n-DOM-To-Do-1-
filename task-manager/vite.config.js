7f558d1bc3f325ecb275ffd8505ef649645f1b75
victoMR <ratonrangelmejia+2@gmail.com>
2025-03-26 14:24:31 -0600

    fix url backend

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Backend URL
+        target: 'https://manipulaci-n-dom-to-do-1.onrender.com', // Backend URL
         changeOrigin: true,
         secure: false,
       },
